a6c6306e302f155af5564c02fcb29ca4c8f02d17
hippocookie <zesheng.zong@outlook.com>
2024-06-28 01:58:17 +0000

    Load acl lib libascendcl.so libgraph.so from ascend-toolkit to build executable test

--- test/cpp/aten/npu_generator_test.cpp
+++ test/cpp/aten/npu_generator_test.cpp
@@ -4,19 +4,21 @@
 using namespace at_npu;
 
 TEST(NPUGeneratorTest, TestSingletonDefaultGenerator) {
-    auto gen = detail::getDefaultNPUGenerator();
-    auto other = detail::getDefaultNPUGenerator();
-    EXPECT_EQ(gen, other);
+  auto gen = detail::getDefaultNPUGenerator();
+  auto other = detail::getDefaultNPUGenerator();
+  EXPECT_EQ(gen, other);
 }
 
 TEST(NPUGeneratorTest, TestCloning) {
-    auto gen1 = detail::createNPUGenerator();
-    auto npu_gen1 = at::check_generator<NPUGeneratorImpl>(gen1);
+  auto gen1 = detail::createNPUGenerator();
+  auto npu_gen1 = at::check_generator<NPUGeneratorImpl>(gen1);
 
-    auto gen2 = detail::createNPUGenerator();
-    gen2 = gen1.clone();
-    auto npu_gen2 = at::check_generator<NPUGeneratorImpl>(gen2);
-    
-    EXPECT_EQ(npu_gen1->current_seed(), npu_gen2->current_seed());
-    EXPECT_EQ(npu_gen1->philox_offset_per_thread(), npu_gen2->philox_offset_per_thread());
+  auto gen2 = detail::createNPUGenerator();
+  gen2 = gen1.clone();
+  auto npu_gen2 = at::check_generator<NPUGeneratorImpl>(gen2);
+
+  EXPECT_EQ(npu_gen1->current_seed(), npu_gen2->current_seed());
+  EXPECT_EQ(
+      npu_gen1->philox_offset_per_thread(),
+      npu_gen2->philox_offset_per_thread());
 }
