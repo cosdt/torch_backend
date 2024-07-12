674ec24c0da842d64a64fafe9ddd914cc11f4355
Yuanhao Ji <jiyuanhao@apache.org>
2024-07-12 09:33:02 +0000

    update [skip ci]

--- test/cpp/core/device.cpp
+++ test/cpp/core/device.cpp
@@ -2,7 +2,7 @@
 
 #include "csrc/core/impl/PrivateUse1GuardImpl.h"
 
-TEST(DeviceGuardTest, First) {
+TEST(DeviceGuardTest, StaticType) {
   EXPECT_EQ(
       c10_backend::impl::PrivateUse1GuardImpl::static_type,
       c10::DeviceType::PrivateUse1);
