8b3629f954bbfa484e136954291b46386972fab7
hippocookie <zesheng.zong@outlook.com>
2024-07-22 06:34:25 +0000

    update

--- cmake/Dependencies.cmake
+++ cmake/Dependencies.cmake
@@ -2,13 +2,10 @@
 if(BUILD_TEST)
   enable_testing()
   add_subdirectory(${PROJECT_SOURCE_DIR}/third_party/googletest)
-  include_directories(
-    BEFORE SYSTEM
-    ${PROJECT_SOURCE_DIR}/third_party/googletest/googletest/include)
-  include_directories(
-    BEFORE SYSTEM
-    ${PROJECT_SOURCE_DIR}/third_party/googletest/googlemock/include)
+  include_directories(BEFORE SYSTEM ${PROJECT_SOURCE_DIR}/third_party/googletest/googletest/include)
+  include_directories(BEFORE SYSTEM ${PROJECT_SOURCE_DIR}/third_party/googletest/googlemock/include)
 
   # test/cpp/core
-  add_subdirectory(${PROJECT_SOURCE_DIR}/test/cpp ${CMAKE_BINARY_DIR}/test_core)
+  add_subdirectory(${PROJECT_SOURCE_DIR}/test/cpp/core ${CMAKE_BINARY_DIR}/test_core)
+  add_subdirectory(${PROJECT_SOURCE_DIR}/test/cpp/npu ${CMAKE_BINARY_DIR}/test_npu)
 endif()
