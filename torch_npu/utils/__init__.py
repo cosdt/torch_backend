d9d127ce0e2049348a4944a5f12e72372624d448
zong <54812088+hippocookie@users.noreply.github.com>
2024-08-06 18:50:28 +0800

    Remove redundant utils logic (#62)

--- torch_npu/utils/__init__.py
+++ torch_npu/utils/__init__.py
@@ -1,9 +1,3 @@
 from ._module import _apply_module_patch
 from .tensor_methods import _add_tensor_methods
-from .storage import _add_storage_methods
-from .combine_tensors import npu_combine_tensors, get_part_combined_tensor, is_combined_tensor_valid
-from .npu_intercept import _cann_package_check, _add_intercept_methods
-from .collect_env import _add_collect_env_methods
-from .utils import print_error_log, print_warn_log, print_info_log
 
-__all__ = ["npu_combine_tensors", "get_part_combined_tensor", "is_combined_tensor_valid"]
