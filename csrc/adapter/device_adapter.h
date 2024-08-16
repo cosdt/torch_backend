95388bd5d634eba3e5fdc70f2c34c375ffc0e12d
hippocookie <zesheng.zong@outlook.com>
2024-08-16 07:16:17 +0000

    update

--- csrc/adapter/device_adapter.h
+++ csrc/adapter/device_adapter.h
@@ -1,3 +1,21 @@
+/**
+ * @file device_adapter.h
+ * @brief Defines the DEVICE_NAMESPACE macro and structures for device-related
+ * functionality.
+ *
+ * This header file contains the definition for the DEVICE_NAMESPACE macro and
+ * basic type definitions for device functions. The DEVICE_NAMESPACE macro
+ * is intended to be replaced at compile time with a specific namespace
+ * corresponding to the desired device implementation. This allows for flexible
+ * routing to different implementations of device-related functions at compile
+ * time.
+ *
+ * @note The DEVICE_NAMESPACE macro should be defined via a compile option,
+ * such as `-DDEVICE_NAMESPACE=acl_adapter`, where `acl_adapter` is the
+ * namespace containing the specific device implementation. This compile-time
+ * substitution enables routing to the appropriate device code.
+ */
+
 #pragma once
 
 #include <c10/core/Device.h>
